e7480de143a3406bca173a6bcf61046da19cdbdc
Kenji Saito <ken-yo@mbr.nifty.com>
2020-08-23 13:24:29 +0900

    Added the GitHub action

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # update-node-modules
+
+Updates the node modules by package.json with jq.
+
+## Usage 
+
+```
+- uses: poad/update-node-modules
+　with:
+    # path to package.json
+    # Default: ./
+   path: ./
+    # true if you use yarn, false if you use npm
+    # Default: false
+   yarn: false
+```
---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function main() {
+    if [ -d "${INPUT_PATH}" ]; then
+    	cd ${INPUT_PATH}
+    else
+        if [ -f "${INPUT_PATH}" ]; then
+            DIR=$(dirname "${INPUT_PATH}")
+            cd "${DIR}"
+            if [ -n "package.json" ]; then
+                echo "no package.json found"
+                exit 1;
+            fi
+        else
+            echo "Invalid path ${INPUT_PATH}"
+            exit 1;
+        fi
+    fi
+    # parse package.json
+    dev_modules=$(echo -n $(cat package.json | jq -r ".devDependencies | to_entries | .[].key"))
+    echo ${dev_modules}
+
+    modules=$(echo -n $(cat package.json | jq -r ".dependencies | to_entries | .[].key"))
+    echo ${modules}
+
+    if [ "${INPUT_YARN}" == "true" ]; then
+        yarn add --dev ${dev_modules}
+        yarn add ${modules}
+    else
+        npm install --only=dev ${dev_modules}
+        npm install ${modules}
+	fi
+}
+
+main
